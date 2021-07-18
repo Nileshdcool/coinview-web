b40f0593ed95a47a9139306b0838e05e1a27b8d2
neil.sukalikar <neil.sukalikar@gmail.com>
2021-07-19 00:55:42 +0530

    sentry error handling

--- src/services/AssetService.js
+++ src/services/AssetService.js
@@ -1,39 +1,39 @@
 import http from "../http-common";
 
 const getAll = () => {
-    return http.get("/assets");
+    return http.get("/assets").catch(err=>err);
 };
 
 const getAllAssets = () => {
-    return http.get("/assets/getAllAssets");
+    return http.get("/assets/getAllAssets").catch(err=>err);
 };
 
 const get = id => {
-    return http.get(`/assets/${id}`);
+    return http.get(`/assets/${id}`).catch(err=>err);
 };
 
 const create = data => {
-    return http.post("/assets", data);
+    return http.post("/assets", data).catch(err=>err);
 };
 
 const update = (id, data) => {
-    return http.put(`/assets/${id}`, data);
+    return http.put(`/assets/${id}`, data).catch(err=>err);
 };
 
 const remove = id => {
-    return http.delete(`/assets/${id}`);
+    return http.delete(`/assets/${id}`).catch(err=>err);
 };
 
 const removeAll = () => {
-    return http.delete(`/assets`);
+    return http.delete(`/assets`).catch(err=>err);
 };
 
 const findByName = name => {
-    return http.get(`/assets?name=${name}`);
+    return http.get(`/assets?name=${name}`).catch(err=>err);
 };
 
 const getCryptoInfo = id => {
-    return http.get(`/assets/getCryptoInfo?id=${id}`);
+    return http.get(`/assets/getCryptoInfo?id=${id}`).catch(err=>err);
 };
 
 const AssetService = {
