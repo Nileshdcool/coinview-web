354e10b10a8eccebbb9e00fa2b5b1092bc3c9170
neil.sukalikar <neil.sukalikar@gmail.com>
2021-07-19 01:14:04 +0530

    git push

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://coinview-ai.herokuapp.com/api",
+    baseURL: "http://localhost:8080/api",
     headers: {
         "Content-type": "application/json"
     }
 });
+
+// heroku config
+
+// export default axios.create({
+//     baseURL: "https://coinview-ai.herokuapp.com/api",
+//     headers: {
+//         "Content-type": "application/json"
+//     }
+// });
